d164f9fb60d7ceb6801afde937032fd38e774a17
shrikantsahu2004 <sahu.sr@somaiya.edu>
2021-12-02 22:09:45 +0530

    Login issue solved

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,10 +1,13 @@
 import 'package:assignment_practice/constants.dart';
+import 'package:assignment_practice/models/user_model.dart';
 import 'package:assignment_practice/screens/requests.dart';
 import 'package:assignment_practice/screens/user_bookings.dart';
 import 'package:assignment_practice/widgets/redirect.dart';
 import 'package:assignment_practice/widgets/status_card.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import '../widgets/toggle_switch.dart';
+import '../services/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 
@@ -19,98 +22,112 @@ class Notifications extends StatefulWidget {
 }
 
 class _NotificationsState extends State<Notifications> {
-  auth.User? user = auth.FirebaseAuth.instance.currentUser;
   final userRef = FirebaseFirestore.instance.collection('users');
+
   @override
   int selectedIndex = 0;
   // List<String>? labels = ["My Bookings"];
   Widget build(BuildContext context) {
-    return user == null
-        ? SafeArea(
-            child: LoginRedirect(callBack: widget.goToLogin!),
-          )
-        : StreamBuilder<DocumentSnapshot>(
-            stream: userRef.doc(user!.uid).snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return CircularProgressIndicator();
-              }
+    final authService = Provider.of<AuthService>(context);
+    return StreamBuilder<MyUser?>(
+      stream: authService.user,
+      builder: (_, AsyncSnapshot<MyUser?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final MyUser? user = snapshot.data;
+          if (user == null) {
+            return SafeArea(child: LoginRedirect(callBack: widget.goToLogin!));
+          } else {
+            return StreamBuilder<DocumentSnapshot>(
+                stream: userRef.doc(user.uid).snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
 
-              bool? isOwner = snapshot.data!['isOwner'];
-              return DefaultTabController(
-                initialIndex: 0,
-                length: 2,
-                child: Scaffold(
-                  appBar: AppBar(
-                    backgroundColor: Colors.purple,
-                    title: const Text('Notifications'),
-                    bottom: TabBar(
-                      indicatorColor: Colors.white,
-                      tabs: <Widget>[
-                        Tab(
-                          text: "My Bookings",
+                  bool? isOwner = snapshot.data!['isOwner'];
+                  return DefaultTabController(
+                    initialIndex: 0,
+                    length: 2,
+                    child: Scaffold(
+                      appBar: AppBar(
+                        backgroundColor: Colors.purple,
+                        title: const Text('Notifications'),
+                        bottom: TabBar(
+                          indicatorColor: Colors.white,
+                          tabs: <Widget>[
+                            Tab(
+                              text: "My Bookings",
+                            ),
+                            Tab(
+                              text: "Incoming Requests",
+                            )
+                          ],
                         ),
-                        Tab(
-                          text: "Incoming Requests",
-                        )
-                      ],
-                    ),
-                  ),
-                  body: TabBarView(
-                    children: <Widget>[
-                      UserBookings(),
-                      isOwner == null
-                          ? Container()
-                          : isOwner
-                              ? BookingRequestPage()
-                              : Container(
-                                  decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                        begin: Alignment.topLeft,
-                                        end: Alignment.bottomRight,
-                                        colors: [
-                                          Colors.purple.shade800,
-                                          // Colors.purple.shade600,
-                                          Colors.purple.shade400,
-                                          Colors.purple.shade200,
-                                          // Colors.purple.shade100,
-                                          Colors.deepPurpleAccent,
-                                        ]),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        "Are you a Venue Owner?",
-                                        style: TextStyle(
-                                            color: Colors.white, fontSize: 22),
+                      ),
+                      body: TabBarView(
+                        children: <Widget>[
+                          UserBookings(),
+                          isOwner == null
+                              ? Container()
+                              : isOwner
+                                  ? BookingRequestPage()
+                                  : Container(
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.bottomRight,
+                                            colors: [
+                                              Colors.purple.shade800,
+                                              // Colors.purple.shade600,
+                                              Colors.purple.shade400,
+                                              Colors.purple.shade200,
+                                              // Colors.purple.shade100,
+                                              Colors.deepPurpleAccent,
+                                            ]),
                                       ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(4.0),
-                                        child: ElevatedButton(
-                                            style: ElevatedButton.styleFrom(
-                                                shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            24))),
-                                            onPressed: () {
-                                              widget.redirect!();
-                                            },
-                                            child: Text(
-                                              "Register Venue",
-                                              style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontSize: 18),
-                                            )),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                    ],
-                  ),
-                ),
-              );
-            });
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            "Are you a Venue Owner?",
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 22),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(4.0),
+                                            child: ElevatedButton(
+                                                style: ElevatedButton.styleFrom(
+                                                    shape:
+                                                        RoundedRectangleBorder(
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        24))),
+                                                onPressed: () {
+                                                  widget.redirect!();
+                                                },
+                                                child: Text(
+                                                  "Register Venue",
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 18),
+                                                )),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+          }
+        }
+        return CircularProgressIndicator();
+      },
+    );
     // return Container(
     //   child: CustomToggle(
     //     selectedIndex: selectedIndex,
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -31,151 +31,162 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     final _auth = Provider.of<AuthService>(context);
-    auth.User? user = auth.FirebaseAuth.instance.currentUser;
 
-    return user == null
-        ? SafeArea(child: LoginRedirect(callBack: widget.onClick!))
-        : SafeArea(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: SingleChildScrollView(
-                child: StreamBuilder<DocumentSnapshot?>(
-                    stream: FirebaseFirestore.instance
-                        .collection('users')
-                        .doc(user.uid)
-                        .snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                      return Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            children: [
-                              Container(
-                                padding: EdgeInsets.all(10.0),
-                                child: Center(
-                                  child: CircleAvatar(
-                                    radius: 80.0,
-                                    backgroundImage:
-                                        snapshot.data!['profile'] != null
-                                            ? NetworkImage(
-                                                snapshot.data!['profile'])
-                                            : AssetImage('images/check.jpg')
-                                                as ImageProvider,
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                snapshot.data!['name'] ?? 'Marcus Ericson',
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontFamily: 'NotoSans',
-                                  // fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 0.0,
-                                width: 150.0,
-                                child: Divider(
-                                  color: Colors.purple[900],
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          //Used Extract Widget for this
-                          Container(
-                            margin: EdgeInsets.only(top: 20, bottom: 10),
-                            child: Column(children: [
-                              ProfileCard(
-                                  icon: Icons.email,
-                                  text: snapshot.data!['email'] ??
-                                      "ericson1234@gmail.com"),
-                              ProfileCard(
-                                  icon: Icons.phone,
-                                  text: snapshot.data!['number'] ??
-                                      "+1234 567 891"),
-                            ]),
-                          ),
-
-                          Column(children: [
-                            !snapshot.data!['isOwner']
-                                ? GestureDetector(
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  OwnerRegister(
-                                                      goToHome: widget
-                                                          .changeBottomTab)));
-                                    },
-                                    child: ProfileTile(
-                                      icon: Icons.add,
-                                      text: "Add your own Venue",
+    final authService = Provider.of<AuthService>(context);
+    return StreamBuilder<MyUser?>(
+      stream: authService.user,
+      builder: (_, AsyncSnapshot<MyUser?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final MyUser? user = snapshot.data;
+          if (user == null) {
+            return SafeArea(child: LoginRedirect(callBack: widget.onClick!));
+          } else {
+            return SafeArea(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: SingleChildScrollView(
+                  child: StreamBuilder<DocumentSnapshot?>(
+                      stream: FirebaseFirestore.instance
+                          .collection('users')
+                          .doc(user.uid)
+                          .snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Center(
+                                    child: CircleAvatar(
+                                      radius: 80.0,
+                                      backgroundImage:
+                                          snapshot.data!['profile'] != null
+                                              ? NetworkImage(
+                                                  snapshot.data!['profile'])
+                                              : AssetImage('images/check.jpg')
+                                                  as ImageProvider,
                                     ),
-                                  )
-                                : Container(),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Favourites()),
-                                );
-                              },
-                              child: ProfileTile(
-                                icon: Icons.favorite,
-                                text: "My Favorites",
-                              ),
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                print("FAQ");
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => FAQ(),
                                   ),
-                                );
-                              },
-                              child: ProfileTile(
-                                icon: Icons.question_answer,
-                                text: " FAQ",
+                                ),
+                                Text(
+                                  snapshot.data!['name'] ?? 'Marcus Ericson',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                    fontFamily: 'NotoSans',
+                                    // fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 0.0,
+                                  width: 150.0,
+                                  child: Divider(
+                                    color: Colors.purple[900],
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            //Used Extract Widget for this
+                            Container(
+                              margin: EdgeInsets.only(top: 20, bottom: 10),
+                              child: Column(children: [
+                                ProfileCard(
+                                    icon: Icons.email,
+                                    text: snapshot.data!['email'] ??
+                                        "ericson1234@gmail.com"),
+                                ProfileCard(
+                                    icon: Icons.phone,
+                                    text: snapshot.data!['number'] ??
+                                        "+1234 567 891"),
+                              ]),
+                            ),
+
+                            Column(children: [
+                              !snapshot.data!['isOwner']
+                                  ? GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    OwnerRegister(
+                                                        goToHome: widget
+                                                            .changeBottomTab)));
+                                      },
+                                      child: ProfileTile(
+                                        icon: Icons.add,
+                                        text: "Add your own Venue",
+                                      ),
+                                    )
+                                  : Container(),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Favourites()),
+                                  );
+                                },
+                                child: ProfileTile(
+                                  icon: Icons.favorite,
+                                  text: "My Favorites",
+                                ),
                               ),
-                            ),
-                            ProfileTile(
-                              icon: Icons.settings,
-                              text: "Settings",
-                            ),
-                            GestureDetector(
-                              onTap: () async {
-                                try {
-                                  // Provider.of<MyAuth>(context, listen: false)
-                                  //     .setIsDone(false);
-                                  // Provider.of<MyAuth>(context, listen: false)
-                                  //     .setUser(null);
-                                  _auth.signOut();
-                                } catch (err) {
-                                  print("Error");
-                                  print(err);
-                                }
-                              },
-                              child: ProfileTile(
-                                icon: Icons.logout,
-                                text: "Logout",
+                              GestureDetector(
+                                onTap: () {
+                                  print("FAQ");
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => FAQ(),
+                                    ),
+                                  );
+                                },
+                                child: ProfileTile(
+                                  icon: Icons.question_answer,
+                                  text: " FAQ",
+                                ),
                               ),
-                            ),
-                          ])
-                        ],
-                      );
-                    }),
+                              ProfileTile(
+                                icon: Icons.settings,
+                                text: "Settings",
+                              ),
+                              GestureDetector(
+                                onTap: () async {
+                                  try {
+                                    // Provider.of<MyAuth>(context, listen: false)
+                                    //     .setIsDone(false);
+                                    // Provider.of<MyAuth>(context, listen: false)
+                                    //     .setUser(null);
+                                    _auth.signOut();
+                                  } catch (err) {
+                                    print("Error");
+                                    print(err);
+                                  }
+                                },
+                                child: ProfileTile(
+                                  icon: Icons.logout,
+                                  text: "Logout",
+                                ),
+                              ),
+                            ])
+                          ],
+                        );
+                      }),
+                ),
               ),
-            ),
-          );
+            );
+          }
+        }
+        return CircularProgressIndicator();
+      },
+    );
   }
 }
